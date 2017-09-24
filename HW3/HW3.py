cbc41abc6e285f3f0e770c972461fc7f57e2adfd
MahnazBehroozi <mbehroo@ncsu.edu>
2017-09-24 00:48:55 -0400

    Update HW3.py

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -15,6 +15,7 @@ def Unsupervised(length):
     binIndeces = []
     i = int(binSize)
     startIndex = 0
+    counter = 1
     while i < n-1:
         while span(data,startIndex,i) < epsilon:
             if i+1 > n-1:
@@ -24,20 +25,57 @@ def Unsupervised(length):
         binIndeces.append(i)
         
         if (i + int(binSize) - 1) < n-1:
-            print "Range size = %d" %(i-startIndex), ", span = %f" %span(data,startIndex,i), ", lo = %.10f" % min(data[startIndex:i]), ", hi = %.10f" % max(data[startIndex:i])
+            print "%d:"%counter, "Range size = %d" %(i-startIndex), ", span = %f" %span(data,startIndex,i), ", lo = %.10f" % min(data[startIndex:i]), ", hi = %.10f" % max(data[startIndex:i])
+            counter = counter + 1
             startIndex = i 
             i = i + int(binSize) 
         else: 
-            print "Range size = %d" %(len(data)-startIndex), ", span = %f" %span(data,startIndex,i), ", lo = %.10f" % min(data[startIndex:i]), ", hi = %.10f" % max(data[startIndex:i])
+            print "%d:"%counter, "Range size = %d" %(len(data)-startIndex), ", span = %f" %span(data,startIndex,i), ", lo = %.10f" % min(data[startIndex:i]), ", hi = %.10f" % max(data[startIndex:i])
             break
             
         
-    return binIndeces
+    return binIndeces,data
 
 
 
-def supervised(data):
-    pass
+def supervised(data,labels):
+    ranges = []
+    for i in range(len(labels)+1):
+        globals()['range%s' % str(i+1)] = []
+    number = 2
+    i = 0
+    for j in range(len(labels)):
+    
+        while j==0 and data[i] < labels[j]:
+            range1.append(data[i])
+            i = i + 1
+            if i > len(data)-1:
+                break
+        if j == 0:
+            ranges.append(range1)
+                
+        while i < len(data)-1 and j < len(labels)-1 and labels[j] <= data[i] and data[i] < labels[j+1]:
+            globals()['range%s' % str(number)].append(data[i])
+            i = i + 1
+            if i > len(data)-1:
+                    break 
+        if number < len(labels) + 1:     
+            ranges.append(globals()['range%s' % str(number)])
+            number = number + 1
+            
+        while j == len(labels) - 1 and data[i] >= labels[j]:
+            globals()['range%s' % str(number)].append(data[i])
+            i = i + 1
+            if i > len(data)-1:
+                break
+
+    ranges.append(globals()['range%s' % str(number)])
+    counter = 1
+    for i in range(len(ranges)):
+        print "Label = %d:"%counter, "most = %.10f" %ranges[i][len(ranges[i])-1]
+        counter = counter + 1
+        
+    return ranges
 
 
 def span(data,startingIndex, endIndex):
@@ -54,4 +92,7 @@ def generator(n):
 
 
 ###########################################################################
-Unsup = Unsupervised(50)
+print "Unsupervised ranges:\n"
+[Unsup, data] = Unsupervised(50)
+print "\n Supervised ranges:\n"
+sup = supervised(data, [0.2,0.6,0.9])
